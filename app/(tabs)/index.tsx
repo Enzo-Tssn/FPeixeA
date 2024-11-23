be8f8395e30ac3f7c897d5e416240cc175b83e8b
Enzo <enzo.neves@usp.br>
2024-11-23 01:06:03 -0300

    final changes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,12 +5,41 @@ import { Alert, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-na
 export default function App() {
   const [temperatura, setTemperatura] = useState('Carregando...');
   const [timerAlimentacao, setTimerAlimentacao] = useState('Carregando...');
+  const [alertaTemp, setAlertaTemp] = useState('Temperatura');
 
   const esp32Url = 'http://192.168.101.32/';
 
   const fetchData = async () => {
-    // Código para buscar dados omitido para simplificar
+    try {
+      const response = await axios.get(esp32Url);
+      if (response.status === 200) {
+        const data = response.data;
+  
+        // Formatar a temperatura para ter uma casa decimal
+        const formattedTemperatura = data.temperatura.toFixed(1); // Garante uma casa decimal
+  
+        // Formatar o tempo para ter dois dígitos para hora, minuto e segundo
+        const formattedHora = data.timerHora.toString().padStart(2, '0'); // Garante dois dígitos
+        const formattedMinuto = data.timerMinuto.toString().padStart(2, '0');
+        const formattedSegundo = data.timerSegundo.toString().padStart(2, '0');
+  
+        if (formattedTemperatura < 24) setAlertaTemp('⚠️ Temperatura muito baixa!');
+        else setAlertaTemp('Temperatura');
+
+        setTemperatura(`${formattedTemperatura} °C`);
+        setTimerAlimentacao(`${formattedHora}h${formattedMinuto}m${formattedSegundo}s`);
+      } else {
+        Alert.alert('Erro', 'Falha ao buscar dados');
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        Alert.alert('Erro', `Falha ao buscar dados: ${error.message}`);
+      } else {
+        Alert.alert('Erro', 'Ocorreu um erro desconhecido.');
+      }
+    }
   };
+  
 
   const alimentar = async () => {
     try {
@@ -40,16 +69,19 @@ export default function App() {
   return (
     <View style={styles.container}>
       {/* Logo */}
-      <Text style={styles.logo}>FPeixeA</Text>
+      <Image
+        source={require('@/assets/images/logo.png')}
+        style={styles.logo}
+      />
 
       {/* Cards */}
       <View style={styles.card}>
-        <Text style={styles.label}>Temperatura</Text>
-        <Text style={styles.value}>{temperatura} Celsius</Text>
+        <Text style={styles.label}>{alertaTemp}</Text>
+        <Text style={styles.value}>{temperatura}</Text>
       </View>
 
       <View style={styles.card}>
-        <Text style={styles.label}>Último Alimento</Text>
+        <Text style={styles.label}>Última alimentação há</Text>
         <Text style={styles.value}>{timerAlimentacao}</Text>
       </View>
 
@@ -60,7 +92,7 @@ export default function App() {
 
       {/* Imagem das ondas na parte inferior */}
       <Image
-        source={require('@/assets/images/waves.png')} // Substitua pelo caminho da sua imagem
+        source={require('@/assets/images/waves.png')}
         style={styles.waves}
         resizeMode="cover" // Ajusta a imagem para cobrir o espaço
       />
@@ -72,20 +104,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#82CFFD',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
+    paddingTop: 72,
   },
   logo: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#fff',
-    marginBottom: 40,
+    width: 120,
+    height: 120,
+    marginBottom: 16,
   },
   card: {
     backgroundColor: 'rgba(255, 255, 255, 0.8)',
     borderRadius: 10,
     padding: 20,
-    marginVertical: 10,
+    marginVertical: 24,
     width: '80%',
     alignItems: 'center',
   },
@@ -100,7 +132,7 @@ const styles = StyleSheet.create({
     marginTop: 5,
   },
   button: {
-    backgroundColor: '#4682B4',
+    backgroundColor: '#0939B0',
     paddingVertical: 15,
     paddingHorizontal: 50,
     borderRadius: 25,
